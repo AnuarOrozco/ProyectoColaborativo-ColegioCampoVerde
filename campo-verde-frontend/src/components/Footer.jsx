ec629136849ca4b296177cee6f49f3a58536fe30
AnuarOrozco <anuarorozzco@gmail.com>
2025-05-06 23:17:08 -0600

    feat(footer): add main footer with copyright

---
+++ campo-verde-frontend/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <footer className="bg-green-700 text-white py-6 text-center">
+        <p>&copy; {new Date().getFullYear()} Colegio Campo Verde. Todos los derechos reservados.</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
